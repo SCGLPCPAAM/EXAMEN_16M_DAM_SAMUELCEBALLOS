c0d9923fb2f6351f504a18469c4ef74ab2df89b9
ALUMNOS <ALUMNOS@P503>
2022-05-16 09:30:31 +0200

    commit

---
+++ src/Examen.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Examen {
+
+	static public void main (String args[]) {
+		int n;
+		Scanner in = new Scanner(System.in);
+		System.out.println("Buenos días Ceinmark, soy Samuel Ceballos.");
+		
+		System.out.println("Entregame un número y te diré si es primo.");
+		n = Integer.parseInt(in.nextLine());
+		
+		if (n%2 == 0) {
+			System.out.println("Es primo");
+		}
+		else {
+			System.out.println("No es primo");
+		}
+		
+	}
+}
